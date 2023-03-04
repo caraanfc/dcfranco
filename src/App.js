f643bfd555b04711ca88490c89b55157638211bc
Franco Caraan <franco.caraan@arcadian.la>
2023-03-04 21:42:34 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
+import CatDirectory from './components/cat-directory/cat-directory';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+  const categories = [ 
+    {
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "Womens",
+      "imageUrl": "https://a-static.besthdwallpaper.com/gorgeous-korean-celeb-iu-lee-ji-eun-wallpaper-3440x1440-71031_15.jpg"
+    },
+    {
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "https://64.media.tumblr.com/d8770ec6b9c7b5fc6098497bd1fa1a29/tumblr_nk3lllIyvW1qeqho3o1_1280.jpg"
+    }
+  ]
+
+  return(
+    <CatDirectory categories={categories}/>
+  )
+};
 
 export default App;
---
+++ src/components/cat-directory/cat-directory.jsx
@@ -0,0 +1,16 @@
+import './cat-directory.styles.scss'
+import CategoryItem from '../category-item/cat-item';
+
+const CatDirectory = ({categories}) =>{
+    
+      return (
+    
+        <div className="categories-container">
+          {categories.map((category) => 
+          <CategoryItem key = {category.id} category = {category}/>
+          )}
+        </div>
+      );
+}
+
+export default CatDirectory;
